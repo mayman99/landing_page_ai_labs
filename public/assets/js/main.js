114c46bff9f31b07e63caf70af20df50fb611c9c
Mohamad <no.future2e1@gmail.com>
2023-11-26 17:01:17 +0300

    init commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,258 +1,166 @@
-/**
-* Template Name: Arsha
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
-  "use strict";
+const baseURL = "http://92.220.132.213:40045"
+const upscaleAPI = baseURL + "/sdapi/v1/upscale";
+const statusAPI = baseURL + "/sdapi/v1/progress";
+// progress bar
+const progressBar = document.getElementById('progress_bar');
+const statusText = document.getElementById('status');
+const download_btn = document.getElementById('download_btn');
+const downloadLink = document.getElementById('downloadLink');
+const processing_status_row = document.getElementById('processing_status_row');
+const results_row = document.getElementById('results_row');
+const scaling_factor_radio = document.getElementsByName('inlineRadioOptions');
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+const status = ['Uploading', 'In Queue', 'Pre Processing', 'Scaling', 'Post Processing', 'Ready for Download'];
+let application_state = 0;
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
+// Clean everything to be ready for a second run
+function clean() {
+  download_btn.disabled = true;
+  results_row.style.display = 'none';
+  processing_status_row.style.display = 'none';
+}
 
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
+async function fetchData() {
+  application_state = 1;
+  // Start updating the status once the second HTTP POST call is made
+  console.log('Get status first called');
+  getStatus();
+  processing_status_row.style.display = 'block';
+  const clientId = getCookie('client_id');
+  const form = document.getElementById('uploadForm');
+  const formData = new FormData(form);
+  // First HTTP POST call
+  const response1 = await fetch('/upload', {
+    method: 'POST',
+    body: formData,
   });
+  const file_json = await response1.json();
 
-  /**
-   * Preloader
-   */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+  console.log('Data from endpoint1:', file_json);
+  const file_key = file_json.key;
+  var scale_factor = 2;
 
-  /**
-   * Initiate  glightbox 
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
-
-  /**
-   * Skills animation
-   */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
+  for (var i = 0, length = scaling_factor_radio.length; i < length; i++) {
+    if (scaling_factor_radio[i].checked) {
+      scale_factor = scaling_factor_radio[i].value;
+      break;
     }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
+  }
+  fetch(upscaleAPI, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+    body: JSON.stringify({
+      "resize_mode": 1,
+      "show_extras_results": true,
+      "gfpgan_visibility": 0.005,
+      "codeformer_visibility": 0.005,
+      "codeformer_weight": 0.005,
+      "upscaling_resize": scale_factor,
+      "upscaling_resize_w": 1024,
+      "upscaling_resize_h": 1024,
+      "upscaling_crop": true,
+      "upscaler_1": "R-ESRGAN 4x+",
+      "upscaler_2": "R-ESRGAN 4x+",
+      "extras_upscaler_2_visibility": 0,
+      "upscale_first": false,
+      "imagePath": file_key,
+      "client_id": clientId
+    })
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error("HTTP error " + response.status);
     }
+    return response.json();
+  }).then(data => {
+    console.log(data);
+    results_row.style.display = 'block';
+    if (data.imagePath) {
+      const image_name = data.imagePath;
+      const extensionIndex = image_name.lastIndexOf(".");
+      const newFileName = `${image_name.slice(0, extensionIndex)}_result.png`;
+      downloadLink.href = '/download/' + newFileName;
+    }
+  }).catch(function() {
+    console.log("Fetch error");
   });
+}
 
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
+async function getStatus() {
+  try {
+    const clientId = getCookie('client_id');
+    const response = await fetch(`${statusAPI}?client_id=${clientId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
     });
-  });
 
-})()
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      const data = await response.json();
+      console.log('Data from endpoint3:', data);
+
+      if (data.client_position === -1){
+        if (application_state === 1) {
+          console.log('Uploading');
+          statusText.innerHTML = 'Uploading';
+          progressBar.style.width = '10%';
+          setTimeout(getStatus, 300);
+        } else if (application_state !== 0) {
+          progressBar.style.width = '100%';
+          console.log('complete');
+          statusText.innerHTML = 'Ready for Download';
+          download_btn.disabled = false;
+          application_state = 0;
+        }
+      } 
+      else if (data.client_position === 0) {
+        console.log('Position in Queue is 0');
+        console.log('doing', data.state.job);
+        statusText.innerHTML = data.state.job;
+        application_state = 2;
+        if (data.state.job === 'Preprocessing') {
+          progressBar.style.width = '25%';
+        }
+        else if (data.state.job === 'Scaling') {
+          progressBar.style.width = '40%';
+        }
+        else if (data.state.job === 'Post Processing') {
+          progressBar.style.width = '80%';
+        }
+        else if (data.state.job === 'Writting') {
+          progressBar.style.width = '95%';
+        }
+        setTimeout(getStatus, 300);
+      } else {
+        progressBar.style.width = '20%';
+        statusText.innerHTML = 'Position in Queue is ' + data.client_position.toString();
+        console.log('Position in Queue is ', data.client_position);
+        application_state = 1;
+        setTimeout(getStatus, 300);
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+download_btn.addEventListener('click', downloadImage);
+function downloadImage() {
+  downloadLink.click();
+  clean();
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split('=');
+    if (cookieName === name) {
+      return cookieValue;
+    }
+  }
+  return null;
+}
--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
+const uuid = require('uuid');
+
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const path = require("path");
+const SDAPI_URL = "http://92.220.132.213:40045/sdapi/v1/upscale";
 
 /* Setup application */
 const app = express();
@@ -12,9 +19,7 @@ app.use(cors());
 
 app.use(cors({
     origin: '*'
-  }));
-
-  
+}));
 /* Setup view engine EJS, body-parser and express-static */
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -22,32 +27,17 @@ app.use(express.static("public"));
 
 /* Setup session */
 app.use(
-    session({
-        secret: 'secretly',
-        resave: false,
-        saveUninitialized: false,
-    })
+session({
+    secret: 'secretly',
+    resave: false,
+    saveUninitialized: false,
+})
 );
 
 app.get('/robots.txt', function (req, res) {
     res.type('text/plain');
     res.send("User-agent: *\nAllow: /texture\nAllow: / ");
 });
-
-
-// app.get('/sitemap.xml', async function(req, res, next){
-//     let xml_content = [
-//       '<?xml version="1.0" encoding="UTF-8"?>',
-//       '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-//       '  <url>',
-//       '    <loc>https://www.designfast.app/</loc>',
-//       '    <lastmod>2023-11-11</lastmod>',
-//       '  </url>',
-//       '</urlset>'
-//     ]
-//     res.set('Content-Type', 'text/xml')
-//     res.send(xml_content.join('\n'))
-//   })
 
 console.log(process.env.PORT);
 /* Start the Server */
@@ -56,8 +46,92 @@ const server = http.createServer(app, (req,res) => {
 })
 .listen(process.env.PORT || 3000, () => console.log("Server is Running"));
 
-
 /* Render LandingPage */
 app.get("/", (req, res) => {
+    if (!req.session.client_id) {
+        req.session.client_id = uuid.v4();
+      }
+
+    const clientId = req.session.client_id;
+    // Set the client_id as a cookie in the response
+    res.cookie('client_id', clientId);
+
+    // Set appropriate headers and send the response
+    // res.setHeader('Content-Type', 'text/html');
+    // res.setHeader('Set-Cookie', `client_id=${clientId}; Path=/`);
+    console.log(clientId);
     res.render("index")
-})
+})  
+
+// CONFIGURATION OF S3
+AWS.config.update({
+    secretAccessKey: 'AoZB1aSQDjspP3XfzFxY4L/Zgis2ZNckS0fq7HPi',
+    accessKeyId: "AKIAT4UQTLJVAI4GD256",
+    region: 'us-east-1'
+});
+
+// CREATE OBJECT FOR S3
+const S3 = new AWS.S3();
+
+// CREATE MULTER FUNCTION FOR UPLOAD
+var upload = multer({
+    // CREATE MULTER-S3 FUNCTION FOR STORAGE
+    storage: multerS3({
+        s3: S3,
+        // bucket - WE CAN PASS SUB FOLDER NAME ALSO LIKE 'bucket-name/sub-folder1'
+        bucket: 'satupscale',
+        // META DATA FOR PUTTING FIELD NAME
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        // SET / MODIFY ORIGINAL FILE NAME
+        key: function (req, file, cb) {
+            // cb(null, file.originalname); //set unique file name if you wise using Date.toISOString()
+            // EXAMPLE 1
+            // cb(null, Date.now() + '-' + file.originalname);
+            // EXAMPLE 2
+            cb(null, new Date().toISOString() + '-' + file.originalname);
+        }
+    }),
+    // SET DEFAULT FILE SIZE UPLOAD LIMIT
+    limits: { fileSize: 1024 * 1024 * 1000 }, // 1000MB
+    // FILTER OPTIONS LIKE VALIDATING FILE EXTENSION
+    fileFilter: function(req, file, cb) {
+        const filetypes = /jpeg|jpg|png|tif/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = filetypes.test(file.mimetype);
+        if (mimetype && extname) {
+            return cb(null, true);
+        } else {
+            cb("Error: Allow images only of extensions jpeg|jpg|png|tif !");
+        }
+    }
+});
+
+// Download route
+app.get('/download/:fileName', (req, res) => {
+    const fileName = req.params.fileName;
+  
+    const params = {
+      Bucket: 'satupscale',
+      Key: fileName,
+    };
+  
+    // Get the object from S3 and send it as an attachment
+    S3.getObject(params, (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Error downloading file.' });
+      }
+  
+      res.attachment(fileName);
+      res.send(data.Body);
+    });
+  });
+
+app.post('/upload', upload.single('image'), function (req, res, next) {
+    console.log(req);
+    console.log('Uploaded!');
+    res.send(req.file);
+});
+
