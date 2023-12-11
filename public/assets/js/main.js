16d88f4ece7c9166f0c79e0a6c3a01636393a248
Mohamad <no.future2e1@gmail.com>
2023-12-11 19:44:12 +0300

    latest

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,14 @@
 // const baseURL = "http://92.220.132.213:40045"
 // const baseURL = "https://d6910820daa95904e7.gradio.live"
 // const baseURL = "http://localhost:7860"
-const baseURL = "https://2b4f-178-240-36-65.ngrok-free.app"
+const baseURL = "https://9b56-130-250-185-236.ngrok-free.app"
 const upscaleAPI = baseURL + "/sdapi/v1/upscale";
 const preUpscaleAPI = baseURL + "/sdapi/v1/upscale-preview";
 const statusAPI = baseURL + "/sdapi/v1/progress";
 
 // progress bar
+const main_container = document.getElementById('main_container');
+const main_card = document.getElementById('main_card');
 const progressBar = document.getElementById('progress_bar');
 const statusText = document.getElementById('status');
 const download_btn = document.getElementById('download_btn');
@@ -16,10 +18,13 @@ const results_row = document.getElementById('results_row');
 const preview_row = document.getElementById('preview_row');
 const coffee_row = document.getElementById('coffee_row');
 const scaling_factor_radio = document.getElementsByName('inlineRadioOptions');
-const example_row = document.getElementById('example_row');
 const example_image = document.getElementById('example_image');
 const upscale_button = document.getElementById('upscale_button');
 const preview_button = document.getElementById('preview_button');
+const interactive_example_row = document.getElementById('interactive_example_row');
+const image_input_row = document.getElementById('image_input_row');
+const scaling_factor_row = document.getElementById('scaling_factor_row');
+let image_extention = 'png';
 
 const status = ['Uploading', 'In Queue', 'Pre Processing', 'Scaling', 'Post Processing', 'Ready for Download'];
 const upscalers = ['R-ESRGAN 4x+', 'R-ESRGAN 4x+ Anime6B', '4x-UltraSharp'];
@@ -30,12 +35,19 @@ let file_url = '';
 
 // Clean everything to be ready for a second run
 function clean() {
+  file_key = '';
+  file_url = '';
+  application_state = 0;
   download_btn.disabled = true;
   results_row.style.display = 'none';
   processing_status_row.style.display = 'none';
   preview_button.style.display = 'block';
   upscale_button.style.display = 'none';
   preview_row.style.display = 'none';
+
+  scaling_factor_row.style.display = 'block';
+  image_input_row.style.display = 'block';
+  interactive_example_row.style.display = 'block';
 }
 
 function getCookie(name) {
@@ -67,44 +79,48 @@ function validateInput() {
   return true;
 }
 
-function createImageComparisonSlider(image1, image2, index) {
+function createImageComparisonSlider(parent, image2, image1, index, img_ext = 'png') {
+
+  parent.style.justifyContent = 'center';
+  parent.style.display = 'flex';
+  parent.style.alignItems = 'center';
 
   // Create the wrapper
   let wrapper = document.createElement('div');
-  wrapper.style.display = 'flex';
+  wrapper.style.marginBottom = '20px';
   wrapper.style.alignItems = 'center';
-  
+  wrapper.style.justifyContent = 'center';
+  wrapper.style.display = 'flex';
+
   // Create the radio button and label
   let radioButton = document.createElement('input');
   radioButton.type = 'radio';
   radioButton.name = 'imageComparisonSlider';
   radioButton.id = 'radio' + index;
   radioButton.value = index;
+  radioButton.style.marginRight = '20px';
   
-  let label = document.createElement('label');
-  label.htmlFor = 'radio' + index;
-  label.textContent = ' - Upscaler ' + (index + 1).toString();
-
-  // Add the radio button and label to the wrapper
   wrapper.appendChild(radioButton);
-  wrapper.appendChild(label);
 
   // Create the container and images
   let container = document.createElement('div');
-  container.style.width = '700px';
-  container.style.height = '400px';
   container.style.position = 'relative';
   container.style.overflow = 'hidden';
+  container.style.width = '450px';
+  container.style.height = '300px';
 
   let img1 = document.createElement('img');
-  img1.src = "data:image/png;base64,"+ image1;
+  img1.src = `data:image/${img_ext};base64,${image1}`;
   img1.style.position = 'absolute';
   img1.style.width = '100%';
   img1.style.height = '100%';
   img1.style.objectFit = 'cover';
 
+  // container.style.width = img1.naturalWidth.toString() + 'px';
+  // container.style.height = img1.naturalHeight.toString() + 'px';
+
   let img2 = document.createElement('img');
-  img2.src = "data:image/png;base64,"+ image2;
+  img2.src = `data:image/${img_ext};base64,${image2}`;
   img2.style.position = 'absolute';
   img2.style.width = '100%';
   img2.style.height = '100%';
@@ -122,7 +138,74 @@ function createImageComparisonSlider(image1, image2, index) {
   wrapper.appendChild(container);
 
   // Add the wrapper to the body
-  preview_row.appendChild(wrapper);
+  parent.appendChild(wrapper);
+
+  // Set up the event listeners
+  let isDragging = false;
+  let containerRect = container.getBoundingClientRect();
+  img2.style.clipPath = `inset(0 ${containerRect.width * 0.5}px 0 0)`;
+
+  function handleMouseMove(e) {
+    if (!isDragging) return;
+    let containerRect = container.getBoundingClientRect();
+    let x = e.pageX - containerRect.left;
+    x = Math.min(Math.max(x, 0), containerRect.width); // Constrain x to the container
+    bar.style.left = x + 'px';
+    // Update the clip path of the second image
+    img2.style.clipPath = `inset(0 ${containerRect.width - x}px 0 0)`;
+  }
+
+  bar.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    document.addEventListener('mousemove', handleMouseMove);
+  });
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false;
+    document.removeEventListener('mousemove', handleMouseMove);
+  });
+}
+
+function createExampleImageComparisonSlider(parent, image2, image1) {
+
+  parent.style.display = 'flex';
+  parent.style.justifyContent = 'center';
+  parent.style.alignItems = 'center';
+
+  // Create the container and images
+  let container = document.createElement('div');
+  container.style.position = 'relative';
+  container.style.overflow = 'hidden';
+  container.style.width = '450px';
+  container.style.height = '300px';
+
+  let img1 = document.createElement('img');
+  img1.src = image1;
+  img1.style.position = 'absolute';
+  img1.style.objectFit = 'cover';
+  img1.style.width = '100%';
+  img1.style.height = '100%';
+
+  // container.style.width = img1.naturalWidth.toString() + 'px';
+  // container.style.height = img1.naturalHeight.toString() + 'px';
+
+  let img2 = document.createElement('img');
+  img2.src = image2;
+  img2.style.position = 'absolute';
+  img2.style.width = '100%';
+  img2.style.height = '100%';
+  img2.style.objectFit = 'cover';
+
+  let bar = document.createElement('div');
+  bar.id = 'bar';
+
+  // Add the images and bar to the container
+  container.appendChild(img1);
+  container.appendChild(img2);
+  container.appendChild(bar);
+
+  // Add the wrapper to the body
+  parent.appendChild(container);
 
   // Set up the event listeners
   let isDragging = false;
@@ -154,15 +237,18 @@ function get_scaling_method() {
   // Returns the selected upscaler
 
   let imageComparisonSlider = document.getElementsByName('imageComparisonSlider');
-  let selected_index = 0;
+  let selected_index = -1;
   for (var i = 0, length = imageComparisonSlider.length; i < length; i++) { 
     if (imageComparisonSlider[i].checked) {
       selected_index = imageComparisonSlider[i].value;
       return upscalers[selected_index];
     }
-}}
+  }
+  return -1;
+}
 
 async function fetchPreview() {
+  preview_button.disabled = true;
   // determine if the user is uploading an image or using an image url
   const image_url = document.getElementById('image_url').value;
   const image_file = document.getElementById('image_file').value;
@@ -173,7 +259,6 @@ async function fetchPreview() {
   }
 
   application_state = 1;
-  example_row.style.display = 'none';
   // Start updating the status once the second HTTP POST call is made
   // console.log('Get status first called');
   // getStatus();
@@ -184,16 +269,21 @@ async function fetchPreview() {
 
   // get the image from "image_file" input form
 
-  // First HTTP POST call
-  const response1 = await fetch('/upload', {
-    method: 'POST',
-    body: formData,
-  });
-  const file_json = await response1.json();
-  file_key = file_json.key;
   file_url = image_url;
-
-  // const file_key = "2023-12-01T07-11-20-845Z-before_2.jpg"
+  if (image_url === '') {
+    image_extention = image_file.split('.').pop();
+    // First HTTP POST call
+    const response1 = await fetch('/upload', {
+      method: 'POST',
+      body: formData,
+    });
+    const file_json = await response1.json();
+    file_key = file_json.key;
+  }else{
+    image_extention = image_url.split('.').pop();
+    // replace all characters that are not allowed in a directory name
+    // file_url = file_url.replace(/[^a-zA-Z0-9_]/g, '');
+  }
 
   var scale_factor = 2;
   for (var i = 0, length = scaling_factor_radio.length; i < length; i++) {
@@ -225,22 +315,41 @@ async function fetchPreview() {
     const images = data.images;
     const original_image = data.original_image;
     console.log(images);
-    preview_row.style.display = 'block';
-    for (var i = 0; i < images.length; i++) {
-      createImageComparisonSlider(original_image ,images[i], i);
+    // remove preview row div elements
+    let all_divs = preview_row.querySelectorAll("div");
+    for (let index = 0; index < all_divs.length; index++) {
+      all_divs[index].innerHTML = "";
     }
-    preview_button.style.display = 'none';
-    upscale_button.style.display = 'block';
+    for (var i = 0; i < images.length; i++) {
+      createImageComparisonSlider(preview_row, original_image ,images[i], i, image_extention);
+    }
+    previewView();
   }).catch(function() {
     console.log("Fetch error");
   });
 }
 
-async function fetchData() {
+function previewView() {
+  preview_row.style.display = 'block';
+  preview_button.style.display = 'none';
+  upscale_button.style.display = 'block';
+  scaling_factor_row.style.display = 'none';
+  image_input_row.style.display = 'none';
+  interactive_example_row.style.display = 'none';
+}
 
-  // Hide preview row
+function resultReadyView() {
   preview_row.style.display = 'none';
+  preview_button.style.display = 'none';
+  upscale_button.style.display = 'none';
+  scaling_factor_row.style.display = 'none';
+  image_input_row.style.display = 'none';
+  interactive_example_row.style.display = 'none';
+  download_btn.disabled = false;
+  results_row.style.display = 'block';
+}
 
+async function fetchData() {
   // determine if the user is uploading an image or using an image url
   // const image_url = document.getElementById('image_url').value;
   // const image_file = document.getElementById('image_file').value;
@@ -252,7 +361,6 @@ async function fetchData() {
   
 
   application_state = 1;
-  example_row.style.display = 'none';
   // Start updating the status once the second HTTP POST call is made
   // console.log('Get status first called');
   getStatus();
@@ -272,6 +380,10 @@ async function fetchData() {
 
   var scale_factor = 2;
   const upscaler = get_scaling_method();
+  if (upscaler === -1) {
+    alert('Please select an upscaler');
+    return;
+  }
   for (var i = 0, length = scaling_factor_radio.length; i < length; i++) {
     if (scaling_factor_radio[i].checked) {
       scale_factor = scaling_factor_radio[i].value;
@@ -305,8 +417,8 @@ async function fetchData() {
     }
     return response.json();
   }).then(data => {
-    console.log(data);
-    results_row.style.display = 'block';
+    // console.log(data);
+    resultReadyView();
     if (data.imagePath) {
       const image_name = data.imagePath;
       // const extensionIndex = image_name.lastIndexOf(".");
@@ -393,10 +505,14 @@ function downloadImage() {
   clean();
 }
 
+
+///////
+createExampleImageComparisonSlider(interactive_example_row, "/assets/img/before_2.jpg", "/assets/img/after_2.png");
+
 //////////////////////////////// server down timer
 // Set the target date and time
 // var countDownDate = new Date();
-// countDownDate.setFullYear(2023, 11, 9);
+// countDownDate.setFullYear(2023, 11, 10);
 
 // // Update the countdown every 1 second
 // var countdownInterval = setInterval(function() {
@@ -422,5 +538,5 @@ function downloadImage() {
 //   }
 // }, 1000);
 
-// Use the function
-// createImageComparisonSlider('image1.jpg', 'image2.jpg');
+// // Use the function
+// // createImageComparisonSlider('image1.jpg', 'image2.jpg');
